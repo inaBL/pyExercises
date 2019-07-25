5116396edf213f9d221d7af299c4770204d5f0e2
Ina B <53173004+inaBL@users.noreply.github.com>
2019-07-25 14:43:34 +0300

    Function for finding most common letter

--- textExercises/mostCommonLetter.py
+++ textExercises/mostCommonLetter.py
@@ -5,7 +5,6 @@ Input: String, special characters allowed.
 Output: Most common letter (special characters ir whitespace not counted). In case of multiple same frequencies, returns
 letter first in alphabet.
 
-
 Task (from Checkio):
 You should find the most frequent letter in the text. The letter returned must be in lower case.
 While checking for the most wanted letter, casing does not matter, so for the purpose of your search, "A" == "a".
