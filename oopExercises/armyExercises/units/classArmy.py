db78c7a8e111feac52f65edf12fe3df4d8b13efb
iBL <ina.liukkonen@gmail.com>
2019-08-16 13:19:58 +0300

    Refactored Checkio Battle OOP structure; classes separated, complete "dump" in newestCheckioBattle.py

---
+++ oopExercises/armyExercises/units/classArmy.py
@@ -0,0 +1,41 @@
+from .classWarrior import *
+
+
+class Army():
+    def __init__(self):
+        self.units = []
+
+    def add_units(self, warrior_type, number):
+        i = 0
+        try:
+            while i < number:
+                if warrior_type == Warrior:
+                    self.units.append(Warrior())
+                elif warrior_type == Knight:
+                    self.units.append(Knight())
+                elif warrior_type == Defender:
+                    self.units.append(Defender())
+                elif warrior_type == Vampire:
+                    self.units.append(Vampire())
+                i += 1
+        except TypeError:
+            print('You can only add Warrior types, for more information and subtypes see classWarrior.py')
+
+
+class Battle():
+    def __init__(self):
+        pass
+
+    # army battle
+    def fight(self, army1, army2):
+        while len(army1.units) > 0 and len(army2.units) > 0:
+            if fight(army1.units[0], army2.units[0]):
+                army2.units.pop(0)
+            else:
+                army1.units.pop(0)
+
+        return len(army1.units) > 0
+
+
+if __name__ == '__main__':
+    pass
---
+++ oopExercises/armyExercises/units/classWarrior.py
@@ -0,0 +1,60 @@
+
+class Warrior():
+    def __init__(self):
+        self.health = 50
+        self.attack = 5
+
+        self.is_alive = True
+
+
+# Subclasses
+
+class Knight(Warrior):
+    def __init__(self):
+        super().__init__()
+        self.attack = 7
+
+
+class Defender(Warrior):
+    def __init__(self):
+        super().__init__()
+        self.health = 60
+        self.attack = 3
+        self.defence = 2
+
+
+class Vampire(Warrior):
+    def __init__(self):
+        super().__init__()
+        self.health = 40
+        self.attack = 4
+
+    def vampirism(self, dmg):
+        self.health += float(0.5 * dmg)
+
+
+# 1-1 Duel, connects with army fights
+
+def fight(unit_1, unit_2):
+    while True:
+        if unit_2.defence < unit_1.attack:
+            unit_2.health -= unit_1.attack - unit_2.defence
+            if isinstance(unit_1, Vampire):
+                unit_1.vampirism(unit_1.attack - unit_2.defence)
+            if unit_2.health <= 0:
+                unit_2.is_alive = False
+                break
+
+        if unit_1.defence < unit_2.attack:
+            unit_1.health -= unit_2.attack - unit_1.defence
+            if isinstance(unit_2, Vampire):
+                unit_2.vampirism(unit_2.attack - unit_1.defence)
+            if unit_1.health <= 0:
+                unit_1.is_alive = False
+                break
+
+    return unit_1.is_alive
+
+
+if __name__ == '__main__':
+    pass
